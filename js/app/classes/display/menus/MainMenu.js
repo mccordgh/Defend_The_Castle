99be4fc48a94aa50e318fb97c48c22268e14d9c3
Matthew McCord <mattm.eternal@gmail.com>
2016-12-12 13:47:21 -0600

    leaderboard and credits displaying in orange pixel font

--- js/app/classes/display/menus/MainMenu.js
+++ js/app/classes/display/menus/MainMenu.js
@@ -2,16 +2,31 @@ define(['MenuState', 'GameState', 'KeyManager', 'Assets', 'State'], function(Men
 
 	const CURRENT_PATH = window.location.href;
 	var fontSize = 0, countSinceInput = 11, choicePosition = 0, leaderboardsLoaded = false;
+	var leaderBoard = [], credits = [];
 
 	var MainMenu = MenuState.extend({
 		init:function(_handler){
-			var oReq = new XMLHttpRequest();
-			oReq.addEventListener("load", loadBoardsListener);
-			oReq.open("GET", CURRENT_PATH + "/res/leaderboard/leaderboard.json");
-			oReq.send();
+			//Load Leaderboards
+			$.ajax({
+			  dataType: "json",
+			  url: CURRENT_PATH + "res/leaderboard/leaderboard.json",
+			  success: function(data){
+			  	console.log("success:", data);
+			  },
+			  error: function(data){
+			  	console.log("error:", data);
+			  }
+			}).done(function(data) {
+			  leaderBoard = data.leaderboards;
+			});
+
+			//Load Credits
+			credits = getCredits();
+
 			this.assets = Assets.getAssets('title');
 			this.choices = ['play', 'leaderboards', 'credits'];
 			this.handler = _handler;
+			this.view = 'menu';
 		},
 		tick: function(_dt){
 			// console.log("countSinceInput", countSinceInput);
@@ -26,48 +41,109 @@ define(['MenuState', 'GameState', 'KeyManager', 'Assets', 'State'], function(Men
 				_g.fillStyle = "#333";
 				_g.fillRect(0, 0, this.handler.getWidth(), this.handler.getHeight());
 
-				//title screen
-				_g.myDrawImage(this.assets.mainMenu, 0, 0, 1024, 640);
-				//draw cursor
-				_g.myDrawImage(this.assets.pointer, 260, 400 + (choicePosition * 65), 32, 32);
+	      switch(this.view){
+	      	case 'leaderboards':
+	      		let spaces = "xxx";
+		      	for (let i = 0; i < leaderBoard.length; i++){
+			      		let yDraw = 75 + (i * 55);
+			      		if (i < 9) {
+			      			spaces = "  ";
+			      		} else {
+			      			spaces = " ";
+			      		}
+			      		_g.drawText({
+			      		borderColor: 'white',
+			      		fillColor: 'orange',
+			      		text: `${leaderBoard[i].id}${spaces}: ${leaderBoard[i].rank} : ${leaderBoard[i].name} : ${leaderBoard[i].score}`,
+			      		fontSize: 64,
+			      		font: 'serif',
+			      		x: function() {return 100;},
+			      		y: function() {return yDraw;},
+			      		});
+		      	}
+	      		break;
 
+	      	case 'credits':
+		      	for (let i = 0; i < credits.length; i++){
+			      		let yDraw = 50 + (i * 70);
+			      		_g.drawText({
+			      		borderColor: 'white',
+			      		fillColor: 'orange',
+			      		text: credits[i],
+			      		fontSize: 64,
+			      		font: 'serif',
+			      		x: function() {return 50;},
+			      		y: function() {return yDraw;},
+			      		});
+		      	}
+	      		break;
+
+	      	case 'menu':
+						//title screen
+						_g.myDrawImage(this.assets.mainMenu, 0, 0, 1024, 640);
+						//draw cursor
+						_g.myDrawImage(this.assets.pointer, 260, 400 + (choicePosition * 65), 32, 32);
+	      		break;
+	      	default:
+	      		console.log("default case, why?");
+	      		break;
+	      }
+
+	      //lighter grey border
 				_g.strokeStyle = "#666";
 				_g.strokeRect(0, 0, this.handler.getWidth(), this.handler.getHeight());
       }
 		},
 		getInput: function(_dt){
-			if(this.handler.getKeyManager().up || this.handler.getKeyManager().upArrow) {
-				choicePosition -= 1;
-				if(choicePosition === -1)
-					choicePosition = this.choices.length - 1;
-				countSinceInput = 0;
-			} 
-			if (this.handler.getKeyManager().down || this.handler.getKeyManager().downArrow) {
-				choicePosition += 1;
-				if(choicePosition === this.choices.length)
-					choicePosition = 0;
-				countSinceInput = 0;
-			}
-			if(this.handler.getKeyManager().enter) {
-				switch(this.choices[choicePosition]){
-					case 'play':
-						var gameState = new GameState(this.handler);
-						State.setState(gameState);	
-						break;		
-					case 'leaderboards':
-						console.log("show leaderboards!");
-						break;		
-					case 'credits':
-						console.log("show credits!");
-						break;		
+			if (this.view === 'menu') {
+				if(this.handler.getKeyManager().up || this.handler.getKeyManager().upArrow) {
+					choicePosition -= 1;
+					if(choicePosition === -1)
+						choicePosition = this.choices.length - 1;
+					countSinceInput = 0;
+				} 
+				if (this.handler.getKeyManager().down || this.handler.getKeyManager().downArrow) {
+					choicePosition += 1;
+					if(choicePosition === this.choices.length)
+						choicePosition = 0;
+					countSinceInput = 0;
 				}
-			} 
-		},
-		loadBoardsListener(data) {
-		  console.log("~~" + data);
+			}
+			
+			if(this.handler.getKeyManager().enter) {
+				countSinceInput = 0;
+				if (this.view === 'menu')
+					switch(this.choices[choicePosition]){
+						case 'play':
+							var gameState = new GameState(this.handler);
+							State.setState(gameState);	
+							break;		
+						case 'leaderboards':
+							this.view = 'leaderboards';
+							break;		
+						case 'credits':
+							this.view = 'credits';
+							break;		
+					} else {
+						this.view = 'menu';
+				}
+			}
 		}
 	});
 
+	function getCredits(){
+		return [
+			'http://www.opengameart.org/',
+			'characters: Antifarea(PC)',
+			'tiles: Chris Hamons / Medicine Storm	',
+			'tiles: Buch / Keith Karnage',
+			'Castle: Alucard',
+			'',
+			'Thanks to:',
+			'Jamie Nichols for JS game',
+			'engine Youtube tutorial'
+		];
+	}
 
 	return MainMenu;
 
